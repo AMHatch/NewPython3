7c3c4242cbba4d68d74a3f89be9976e5be542720
AndrewHatch <03110h@gmail.com>
2021-09-13 10:35:25 -0400

    homework

--- week1/homework/iterative_programming.py
+++ week1/homework/iterative_programming.py
@@ -54,51 +54,18 @@
 # [[2,9,3],[4,8,1],[5,7,6]]
 
 
-# tried it with a while loop. having trouble getting it to work. see for loop below.  
+matx1 = [[1,5,4],[2,3,6],[4,8,9]]
+matx2 = [[2,9,3],[4,8,1],[5,7,6]]
 
-#                                                               <<<<<REVISIT>>>>>>>
+final_matx = []
 
-# matrix1 = [[1,5,4],[2,3,6],[4,8,9]]
-# matrix2 = [[2,9,3],[4,8,1],[5,7,6]]
+for outter in range(len(matx1)):
+    temp = []
+    for inner in range(len(matx1)):
+        element = matx1[outter][inner] + matx2[outter][inner]
+        temp.append(element)
+    final_matx.append(temp)
 
-# final_matrix=[]
-
-# index = 0
-# while index < len(matrix1):
-#     matrix3 = []
-#     index0 = 0
-#     while index0 < len(matrix1):
-#         final_inner = 0
-#         index1 = 0
-#         while index1 < len(matrix1):
-#             matrix1_inner = matrix1[index1]
-#             matrix2_inner = matrix2[index0]
-#             final_inner = matrix1_inner[index0] * matrix2_inner[index1]
-#             index1+=1
-#         matrix3.append(final_inner)
-#         index0 += 1
-#     final_matrix.append(matrix3)
-#     index +=1
-# print(final_matrix) 
-
-#                    >>>>>>Definately need clarification on this<<<<<<<<<<    
-# matx1 = [[1,5,4],[2,3,6],[4,8,9]]
-# matx2 = [[2,9,3],[4,8,1],[5,7,6]]
-
-# final_matx = []
-
-# for indexA in range(len(matx1)):                                # for indexA in the range of the lenght of matx1 so :(0,1,2)
-#     matx3 = []                                                  # like above, really should be a list since its being wiped after 3 indices
-#     for indexB in range(len(matx1)):                            # for indexb in the range of the lenght of matx1 so :(0,1,2)
-#         inner_matx = 0                                          #values being saved to be appended to matx3
-#         for indexC in range(len(matx1)):                        # for indexc in the range of the lenght of matx1 so :(0,1,2)
-#             matx1_index = matx1[indexC]                         # matx1_index is the mutable indexA of matx1 being saved to be used in equation.|  
-#             matx2_index = matx2[indexB]                         # matx2_index is the mutable indexB of matx2 being saved to be used in equation.|     
-#             inner_matx += matx1_index[indexB] + matx2_index[indexC]           
-#         matx3.append(inner_matx)
-#     final_matx.append(matx3)
-
-# print(final_matx)
 
 #4
 # Given a list of numbers or strings, create a new list containing the same elements as the first list,
@@ -116,15 +83,11 @@
 # letter_list = ['A','E','G','I','O','S','T']
 # number_list = ['4','3','6','1','0','5','7']
 
-# for i in my_list:
-#     # print(i)                                         # i is then letter in the input
+# for i in my_list:                              
 #     if i in letter_list:
-#         i_of_letter = letter_list.index(i)
-#         # print(i_of_letter)                           # i_of _letter is the index of the letters in letter_list that match between the 
-#         # input list and letter list
-#         value = number_list[i_of_letter]
-#                                          # value of the index in number list given from letter list.
-#         orig_index = my_list.index(i) # index of the orignial list (my list) that needs to be replaced with the value from number list.
+#         i_of_letter = letter_list.index(i)               # i_of _letter is the index of the letters in letter_list that match between the                             
+#         value = number_list[i_of_letter]                 # value of the index in number list given from letter list.                              
+#         orig_index = my_list.index(i)                    # index of the orignial list (my list) that needs to be replaced with the value from number list.
 #         my_list[orig_index] = number_list[i_of_letter]   # this is the value of the orignial list that has been replaced by the numbers from number list. 
 # result =''.join(my_list)
 # print(result)
